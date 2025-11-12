a07898efbea17e2a1ff2b3d779a6eb8b28c9769d
forbiddensandwich <Firetruckhunter@gmail.com>
2025-11-12 01:00:57 -0500

    Finish functions to write combo functions

--- automatic_script_teleporting/last_stretch.py
+++ automatic_script_teleporting/last_stretch.py
@@ -2,9 +2,11 @@ from shortest_path import get_position, find_jump_points, find_backward_jump_poi
 
 # Finds the closest point to the target location and returns the coordinates in an array, and the steps required to reach it
 def find_closest_point(target_index, jump_points, direction, filter):
+
     if direction == "Forward":
         step = 0
         step_modifier = 1
+
     else:
         step = -1
         step_modifier = -1
@@ -12,6 +14,7 @@ def find_closest_point(target_index, jump_points, direction, filter):
     steps = []
     coordinates = []
     # print(f"***ALL JUMP POINTS FOR {direction.upper()} DIRECTION {filter.upper()}***")
+
     for point in jump_points:
         y_value = target_index - point
         steps.append(abs(step) + abs(y_value))
@@ -25,29 +28,35 @@ def find_closest_point(target_index, jump_points, direction, filter):
 
 
 def forward_or_backward(f_coordinate, b_coordinate):
+    # print("Forward Coordinate:", f_coordinate)
+    # print("Reverse Coordinate:", b_coordinate)
     if f_coordinate[1] > b_coordinate[1]:
         return b_coordinate[0], b_coordinate[1],  "Backward"
+    
     else: return f_coordinate[0], f_coordinate[1], "Forward"
 
 
 # Compares the steps returned by the forward jump points, and backward, and finds the shortest route to the desired index
 def get_steps(location_name, location_array):
     filter = "All Locations"
-    backward_allowed = False
+    backward_allowed = True
 
     # Probably a better way to check whether this is the filtered array
     if len(location_array) == 6:
         location_array, filter = get_category(location_name, location_array)
-        # Checks if array is in the 'zone' category
-        if filter != "Wild Zones": backward_allowed = False
+    
+    if filter == "Wild Zones" or filter == "All Locations": backward_allowed = False
+
     index = get_position(location_name, location_array)
     f_jp, direction = find_jump_points(location_array)
     # NEEDS FILTERING TO PREVENT BACKWARD JUMP POINTS FROM RUNNING
     b_jp, r_direction = find_backward_jump_points(location_array)
     shortest_coordinates, steps, direction = find_closest_point(index, f_jp, direction, filter)
+
     if backward_allowed:
-        b_coordinates, steps, direction = find_closest_point(index, b_jp, r_direction, filter)
-        shortest_coordinates, steps, direction = forward_or_backward(shortest_coordinates, [b_coordinates, steps], filter, location_name)
+        b_coordinates, bsteps, direction = find_closest_point(index, b_jp, r_direction, filter)
+        shortest_coordinates, steps, direction = forward_or_backward([shortest_coordinates, steps], [b_coordinates, bsteps])
+
     return shortest_coordinates, steps, location_name, filter
     # Returns Coordinates in an array, Steps, Direction, Filter, Location Name
 # Compares the shortest filtered path to the shortest unfiltered path, and returns the shortest route
@@ -59,7 +68,15 @@ def shortest_path(location):
     if fsteps < usteps:
         print(f'***ABSOLUTE SHORTEST PATH FOR: {location.upper()}***\n{filter}\nSteps: {fsteps}\nCoordinates: {filtered_coordinates}\n')
         return filtered_coordinates, location_name, filter
+    
     else:
         print(f'***ABSOLUTE SHORTEST PATH FOR: {location.upper()}***\n{ufilter}\nSteps: {usteps}\nCoordinates: {unfiltered_coordinates}\n')
         return unfiltered_coordinates, location_name, ufilter
+
 # Finds the shortest path for every location
+def all_shortest_paths():
+
+    for i in range(len(locations)):
+        info_set = shortest_path(locations[i])
+
+all_shortest_paths()
--- automatic_script_teleporting/shortest_path.py
+++ automatic_script_teleporting/shortest_path.py
@@ -40,6 +40,7 @@ def find_jump_points(array):
         jump_points.append(jump_length)
     final_jump = len(array) - 7
     jump_points.append(final_jump)
+    # print(jump_points)
     return jump_points, "Forward"
 
 
@@ -54,74 +55,3 @@ def find_backward_jump_points(array):
     # Check just in case 
     jump_points.append(6)
     return jump_points, "Backward"
-
-
-# Finds the closest point to the target location and returns the coordinates in an array, and the steps required to reach it
-# def find_closest_point(target_index, jump_points, direction, filter):
-#     if direction == "Forward":
-#         step = 0
-#         step_modifier = 1
-#     else:
-#         step = -1
-#         step_modifier = -1
-
-#     steps = []
-#     coordinates = []
-    # print(f"***ALL JUMP POINTS FOR {direction.upper()} DIRECTION {filter.upper()}***")
-    # for point in jump_points:
-    #     y_value = target_index - point
-    #     steps.append(abs(step) + abs(y_value))
-    #     coordinates.append([step, y_value])
-        # print(f'Steps: {steps[-1]}\nCoordinates: {coordinates[-1]}\nDirection: {direction}\n')
-        # step += 1 * step_modifier
-
-    # target_index = steps.index(min(steps))
-    # print(f'***SHORTEST JUMP POINT FOR {direction.upper()} DIRECTION***\nOF: {steps[target_index]} steps\nAT: {coordinates[target_index]}\n{filter.upper()}\n')
-    # return coordinates[target_index], steps[target_index], direction
-
-
-# def forward_or_backward(f_coordinate, b_coordinate):
-#     if f_coordinate[1] > b_coordinate[1]:
-#         return [b_coordinate[0], b_coordinate[1],  "Backward"]
-#     else: return [f_coordinate[0], f_coordinate[1], "Forward"]
-
-
-# Compares the steps returned by the forward jump points, and backward, and finds the shortest route to the desired index
-# def get_steps(location_name, location_array):
-#     filter = "All Locations"
-#     backward_allowed = False
-
-    # Probably a better way to check whether this is the filtered array
-    # if len(location_array) == 6:
-    #     location_array, filter = get_category(location_name, location_array)
-    #     # Checks if array is in the 'zone' category
-    #     if filter != "Wild Zones": backward_allowed = False
-    # index = get_position(location_name, location_array)
-    # f_jp, direction = find_jump_points(location_array)
-    # NEEDS FILTERING TO PREVENT BACKWARD JUMP POINTS FROM RUNNING
-    # b_jp, r_direction = find_backward_jump_points(location_array)
-    # shortest_coordinate = find_closest_point(index, f_jp, direction, filter)
-    # if backward_allowed:
-    #     b_coordinate = find_closest_point(index, b_jp, r_direction, filter)
-    #     shortest_coordinate = forward_or_backward(shortest_coordinate, b_coordinate, filter, location_name)
-    # return shortest_coordinate
-    # Returns Coordinates in an array, Steps, Direction, Filter, Location Name
-# Compares the shortest filtered path to the shortest unfiltered path, and returns the shortest route
-
-
-# def shortest_path(location):
-#     unfiltered = get_steps(location, locations)
-#     filtered = get_steps(location, locations_by_type)
-#     if filtered[1] < unfiltered[1]:
-#         print(f'***ABSOLUTE SHORTEST PATH FOR: {location.upper()}***\nFiltered\nSteps: {filtered[1]}\nCoordinates: {filtered[0]}\nDirection: {filtered[2]}\n')
-#         return filtered
-#     else:
-#         print(f'***ABSOLUTE SHORTEST PATH FOR: {location.upper()}***\nUnfiltered\nSteps: {unfiltered[1]}\nCoordinates: {unfiltered[0]}\nDirection: {unfiltered[2]}\n')
-#         return unfiltered
-# Finds the shortest path for every location
-
-
-def all_shortest_paths():
-    for i in range(len(locations)):
-        info_set = shortest_path(locations[i])
-all_shortest_paths()
