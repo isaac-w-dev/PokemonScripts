0838ba3cdc1f1b1737439731c1e9bb4afdff718c
forbiddensandwich <Firetruckhunter@gmail.com>
2025-11-16 14:59:02 -0500

    Changed GPC script

--- automatic_script_teleporting/init.py
+++ automatic_script_teleporting/init.py
@@ -1,13 +1,8 @@
 init = '''init {
     set_led(LED_1, 1);
-    wait(1000);
     set_led(LED_2, 1);
-    wait(1000);
     set_led(LED_3, 1);
-    wait(1000);
     set_led(LED_4, 1);
-    wait(1000);
     set_led(LED_5, 1);
-    wait(1000);
     set_led(LED_6, 1);
 }''' + '\n\n'
